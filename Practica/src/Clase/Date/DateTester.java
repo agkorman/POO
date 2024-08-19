5c469f43b6204b4d3264f99a4a8d93545795d1cd
Agustin Korman <agkorman@itba.edu.ar>
2024-08-19 09:06:57 -0300

    first commit

---
+++ Practica/src/Clase/Date/DateTester.java
@@ -0,0 +1,10 @@
+package Clase.Date;
+
+public class DateTester {
+
+    public static void main(String[] args) {
+        Date d1 = new Date(10,12,2004);
+        System.out.println(d1.getDay());
+    }
+
+}
