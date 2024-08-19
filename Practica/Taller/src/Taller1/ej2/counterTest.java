fa384dc9aaf1ca3fac5196c31057b50d33a318b4
agkorman <agkorman@itba.edu.ar>
2024-08-19 10:48:12 -0300

    terminada guia 1

---
+++ Practica/Taller/src/Taller1/ej2/counterTest.java
@@ -0,0 +1,17 @@
+package Taller1.ej2;
+
+import Taller1.ej3.MultipleCounter;
+
+public class counterTest {
+    public static void main(String[] args) {
+        Counter counter = new MultipleCounter(3); // el constructor solo se usa con new para crear nuevo obejeto
+        System.out.println("Testing Class Counter");
+        System.out.println(counter.getCount());
+        counter.incCount();
+        System.out.println(counter.getCount());
+        counter.decCount();
+        System.out.println(counter.getCount());
+        counter = new Counter(); // Forma de resetear el contador es crear un nuevo objeto con el mismo nombre
+                        // esto pisa la zona de memoria donde estaba el objeto anterior, resetéandolo
+    }
+}
